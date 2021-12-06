72e6a9f047b238978e3afab757c776a01f12a325
Eduardo J Silva <silvaeduardojavier@hotmail.com>
2021-12-06 19:51:16 -0300

    servidor Add db.json

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { Task } from 'src/app/Task';
-import { TASKS } from '../../mock-tasks';
+import { TaskService } from '../../server/task.service';
+import { Task } from '../../Task';
 
 @Component({
   selector: 'app-tasks',
@@ -8,9 +8,14 @@ import { TASKS } from '../../mock-tasks';
   styleUrls: ['./tasks.component.css'],
 })
 export class TasksComponent implements OnInit {
-  tasks: Task[] = TASKS;
+  tasks: Task[] = [];
 
-  constructor() {}
+  constructor(private taskService: TaskService) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    // Like Promise
+    this.taskService.getTasks().subscribe((tasks)=>(
+    this.tasks = tasks 
+    ));
+  }
 }
